12a45d305cd745bfd4fa9ce282702f3e11198f05
ekramulhoqueridoy <158032414+ekramulhoqueridoy@users.noreply.github.com>
2025-01-14 13:50:27 +0600

    added login and signup pages

---
+++ src/Pages/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SignUp = () => {
+    return (
+        <div>
+            <h1>
+                This is SignUP.
+            </h1>
+        </div>
+    );
+};
+
+export default SignUp;
