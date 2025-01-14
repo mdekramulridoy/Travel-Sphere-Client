68b36eccbd358a0180ab47f6dbb5fceaf5ead4f9
ekramulhoqueridoy <158032414+ekramulhoqueridoy@users.noreply.github.com>
2025-01-14 12:27:12 +0600

    added a dummy readme file

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,15 +2,20 @@ import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
 
-
 const Main = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <Navbar></Navbar>
+      </div>
+      <div>
+        <Outlet></Outlet>
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 const About = () => {
     return (
         <div>
+            <Helmet>
+                <title>
+                    About us | Travel Sphere
+                </title>
+            </Helmet>
             <h1>
                 This is about !
             </h1>
